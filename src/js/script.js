3ef5adf47aa426d204aaeb533506b4a778d79a46
Hipjea <pierre.duverneix@gmail.com>
2023-04-28 12:10:37 +0200

    news

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,9 @@ const IMG_FOLDER = "./src/images/";
 function setWindowHeight() {
   var windowHeight = window.innerHeight;
   var header = document.querySelector(".header");
-  header.style.height = windowHeight + "px";
+  if (header) {
+    header.style.height = windowHeight + "px";
+  }
 }
 
 function scrolledIntoViewport() {
@@ -72,30 +74,32 @@ function setScrollerTransform() {
 }
 
 var pattern = document.querySelector("#pattern");
-var onScroll = (function () {
-  var startPos;
+if (pattern) {
+  var onScroll = (function () {
+    var startPos;
 
-  function run() {
-    var fromTop = window.pageYOffset,
-      rect = pattern.getBoundingClientRect(),
-      scrollDelta;
+    function run() {
+      var fromTop = window.pageYOffset,
+        rect = pattern.getBoundingClientRect(),
+        scrollDelta;
 
-    // check if element is in viewport
-    if ((rect.top - window.innerHeight) <= 0 && rect.bottom > 0)
-      startPos = startPos === undefined ? fromTop : startPos;
-    else {
-      startPos = 0;
-      return;
+      // check if element is in viewport
+      if ((rect.top - window.innerHeight) <= 0 && rect.bottom > 0)
+        startPos = startPos === undefined ? fromTop : startPos;
+      else {
+        startPos = 0;
+        return;
+      }
+
+      scrollDelta = (fromTop - startPos) * 0.1; // "speed" per scrolled frame
+      pattern.style.transform = `translateX(-${scrollDelta}px)`;
     }
 
-    scrollDelta = (fromTop - startPos) * 0.1; // "speed" per scrolled frame
-    pattern.style.transform = `translateX(-${scrollDelta}px)`;
-  }
+    run();
 
-  run();
-
-  return run;
-})();
+    return run;
+  })();
+}
 
 function menuActions() {
   var menuTrigger = document.querySelector("#menu-trigger"),
